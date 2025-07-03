Fix Clip and Sign operators
Signed-off-by: Christian Bourjau <christian.bourjau@quantco.com>

onnxruntime/test/python/test_clip.py
@@ -1,17 +1,12 @@
+import numpy as np
 import onnx
 from onnx import TensorProto, helper
+
 import onnxruntime as ort
-import numpy as np
 
 
 def make_clip_model() -> onnx.ModelProto:
-    max_node = helper.make_node(
-        op_type="Constant",
-        domain="ai.onnx",
-        inputs=[],
-        outputs=["max"],
-        value_int=0
-    )
+    max_node = helper.make_node(op_type="Constant", domain="ai.onnx", inputs=[], outputs=["max"], value_int=0)
     clip_node = helper.make_node(
         op_type="Clip",
         domain="ai.onnx",
@@ -33,6 +28,6 @@ def make_clip_model() -> onnx.ModelProto:
 def test_clip():
     sess = ort.InferenceSession(make_clip_model().SerializeToString())
     x = np.asarray([2147483649, 2147483649], np.int64)
-    (res, ) = sess.run(None, {"x": x})
+    (res,) = sess.run(None, {"x": x})
 
     np.testing.assert_array_equal(res, np.clip(x, min=None, max=0))

onnxruntime/test/python/test_sign.py
@@ -1,7 +1,9 @@
+import numpy as np
 import onnx
 from onnx import TensorProto, helper
+
 import onnxruntime as ort
-import numpy as np
+
 
 def make_sign_model() -> onnx.ModelProto:
     node = helper.make_node(
@@ -25,6 +27,6 @@ def make_sign_model() -> onnx.ModelProto:
 def test_sign():
     sess = ort.InferenceSession(make_sign_model().SerializeToString())
     x = np.asarray([2147483649, 2147483649], np.int64)
-    (res, ) = sess.run(None, {"x": x})
+    (res,) = sess.run(None, {"x": x})
 
     np.testing.assert_array_equal(res, np.sign(x))